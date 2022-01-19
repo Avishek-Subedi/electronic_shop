b4b653c401482765dd7de909001349925b229435
Avishek Subedi <avisheksubedi0@gmail.com>
2022-01-19 12:14:58 +0545

    theme

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+final appTheme = ThemeData(
+  primarySwatch: Colors.blueGrey,
+  textTheme: const TextTheme(
+    headline1: TextStyle(
+      fontWeight: FontWeight.w700,
+      fontSize: 24,
+      color: Colors.black,
+    ),
+  ),
+);
